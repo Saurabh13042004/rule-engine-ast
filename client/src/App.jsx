53a9de50a70313f11949cae33413f1f0277435aa
Saurabh Shukla <saurabh1258.be21@chitkarauniversity.edu.in>
2024-10-25 02:57:42 +0530

    building for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import data from './treeData.json';
+
 import CreateRule from './components/CreateRule';
 import EvaluateRule from './components/EvaluateRule';
 import CombineRules from './components/CombineRule';
 
-function renderNode(node) {
-  if (node.type === 'operand') {
-    return (
-      <div className="ml-4 mb-2">
-        <p className="text-gray-700">{node.value}</p>
-      </div>
-    );
-  } else if (node.type === 'operator') {
-    return (
-      <div className="mb-4">
-        <p className="font-bold text-blue-600">{node.value}</p>
-        <div className="ml-6 border-l border-gray-300 pl-4">
-          {node.left && renderNode(node.left)}
-          {node.right && renderNode(node.right)}
-        </div>
-      </div>
-    );
-  }
-}
 
 function App() {
   return (
